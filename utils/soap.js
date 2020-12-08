3b9e08efdec18a053cabf90c92710f1909e7a923
Yen Colon <yendejesusc41@gmail.com>
2020-12-07 22:46:54 -0400

    implementing soap client

---
+++ utils/soap.js
@@ -0,0 +1,12 @@
+const soap = require("soap");
+
+exports.createSoapClient = (url) => {
+  return soap
+    .createClientAsync(url)
+    .then((client) => {
+      return client;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
