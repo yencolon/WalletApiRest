02af8fdc108d0884ee3c31941a49d2348a860381
Yen Colon <yendejesusc41@gmail.com>
2020-12-09 11:57:53 -0400

    common response formatter

--- middleware/xmlResponseFormatter.js
+++ middleware/xmlResponseFormatter.js
@@ -1,6 +1,13 @@
 const CommonResponse = require("../utils/commonResponse");
 
 module.exports = (req, res, next) => {
-  const response = new CommonResponse(res.respXML[0].return.item);
-  res.status(response.code).send(response);
+  if(!Array.isArray(res.respXML)){
+    res.status(500).send({
+      message: 'Error',
+      xml: res.respXML
+    })
+  } 
+
+  res.data = new CommonResponse(res.respXML[0].return.item);
+  res.status(res.data.code).send(res.data);
 };
