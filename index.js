fca81cfeed631fa483a0294ca0addf9f3b93c650
Yen Colon <yendejesusc41@gmail.com>
2020-12-09 11:56:14 -0400

    index app

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const fileStoreOptions = {
+  path: './sessions',
+  minTimeout: 2000,
+};
+
+const cors = require('cors')
+const authRoutes = require('./routes/authRoutes');
+const walletRouter = require('./routes/walletRoutes');
+
+const server = express();
+server.use(session({
+  store: new FileStore(fileStoreOptions),
+  secret: 'supersecrethash',
+  resave: true,
+  saveUninitialized: true
+}));
+
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(bodyParser.json())
+server.use('/auth', authRoutes);
+server.use('/wallet', walletRouter);
+
+
+server.listen(3001, () => {
+  console.log('running on port 3001');
+});
