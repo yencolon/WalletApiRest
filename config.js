8d983db95871eb91b8d1449444352ad64a9848ef
Yen Colon <yendejesusc41@gmail.com>
2020-12-09 18:06:24 -0400

    removing session

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-module.exports =  'http://localhost:8000/api';
+module.exports =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api"
+    : "https://apiwalletsoap.herokuapp.com/api";
--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-const fileStoreOptions = {
-  path: './sessions',
-  minTimeout: 2000,
-};
+// const fileStoreOptions = {
+//   path: './sessions',
+//   minTimeout: 2000,
+// };
 
 const cors = require('cors')
 const authRoutes = require('./routes/authRoutes');
 const walletRouter = require('./routes/walletRoutes');
 
 const server = express();
-server.use(session({
-  store: new FileStore(fileStoreOptions),
-  secret: 'supersecrethash',
-  resave: true,
-  saveUninitialized: true
-}));
+// server.use(session({
+//   store: new FileStore(fileStoreOptions),
+//   secret: 'supersecrethash',
+//   resave: true,
+//   saveUninitialized: true
+// }));
 
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true}));
